68f5c76917b9c4746a2960db491abf9fe925ec55
lwairore <kwairore@gmail.com>
2020-04-17 21:05:19 +0300

    - Display an input field for the user to enter a number.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
+   <div class="container">
+      <h2>Guess the Number !</h2>
+        <div class="card bg-light mb-3">
+           <div class="card-body">
+              <p class="card-text">Guess the computer generated random number between 1 
+                                                                          and 1000.</p>
+           </div>
+        </div>
+       <div>
+         <label>Your Guess: </label>
+         <input type="number" [value]="guess" (input)="guess = $event.target.value" />
+         <button (click)="verifyGuess()" class="btn btn-primary btn-sm">Verify</button>
+         <button (click)="initializeGame()" class="btn btn-warning btn-
+                                                               sm">Restart</button>
+       </div>
+      <div>
+         <p *ngIf="deviation<0" class="alert alert-warning">Your guess is higher.</p>
+         <p *ngIf="deviation>0" class="alert alert-warning">Your guess is lower.</p>
+         <p *ngIf="deviation===0" class="alert alert-success">Yes! That's it.</p>
+      </div>
+      <p class="text-info">No of guesses :
+        <span class="badge">{{noOfTries}}</span>
+      </p>
+  </div> 
     <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'guessthenumber';
+
+
 }
